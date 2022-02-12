16eda8eb5ccdef141c7854f5dee9acaab33b794d
portaljumperjen <portaljumperjen@gmail.com>
2022-02-12 11:38:43 -0800

    main + pause menus; added textmeshpro

---
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static PauseMenu instance;
+
+    public bool gameIsPaused {get; private set;}
+
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private GameObject pauseMenuDefaultPanel;
+    [SerializeField] private GameObject settingsMenuPanel;
+    [SerializeField] private GameObject controlsMenuPanel;
+
+    [SerializeField] private Button continueButton;
+
+    void Awake()
+    {
+        if( instance ){
+            Destroy(gameObject);
+        }
+        else{
+            instance = this;
+        }
+        gameIsPaused = false;
+    }
+
+    public void ResumeGame()
+    {
+        ResetPauseUI();
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    private void ResetPauseUI()
+    {
+        settingsMenuPanel.SetActive(false);
+        controlsMenuPanel.SetActive(false);
+        pauseMenuDefaultPanel.SetActive(true);
+
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        continueButton.Select();
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        gameIsPaused = false;
+        Time.timeScale = 1f;
+        Debug.Log("Returning to Main Menu!");
+        // SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ToggleSettingsMenu(bool set)
+    {
+        settingsMenuPanel.SetActive(set);
+        pauseMenuDefaultPanel.SetActive(!set);
+    }
+
+    public void ToggleControlsMenu(bool set)
+    {
+        controlsMenuPanel.SetActive(set);
+        pauseMenuDefaultPanel.SetActive(!set);
+    }
+}
